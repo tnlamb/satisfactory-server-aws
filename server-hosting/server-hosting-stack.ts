a8fb09611e16f992e4b127661fa5ebb4e031cd91
tnlamb <tnlamb@wisc.edu>
2023-11-15 17:48:05 -0800

    Update server-hosting-stack.ts

--- server-hosting/server-hosting-stack.ts
+++ server-hosting/server-hosting-stack.ts
@@ -67,7 +67,7 @@ export class ServerHostingStack extends Stack {
 
     const server = new ec2.Instance(this, `${prefix}Server`, {
       // 2 vCPU, 8 GB RAM should be enough for most factories
-      instanceType: new ec2.InstanceType("m5a.large"),
+      instanceType: new ec2.InstanceType("m6a.large"),
       // get exact ami from parameter exported by canonical
       // https://discourse.ubuntu.com/t/finding-ubuntu-images-with-the-aws-ssm-parameter-store/15507
       machineImage: ec2.MachineImage.fromSsmParameter("/aws/service/canonical/ubuntu/server/20.04/stable/current/amd64/hvm/ebs-gp2/ami-id"),
